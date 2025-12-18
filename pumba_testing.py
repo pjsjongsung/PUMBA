f4430d31308da5f8480cc9d8414d55067ad64b73
Jong Sung Park <jp109@h2.quartz.uits.iu.edu>
2025-12-18 15:56:59 -0500

    Fixing test and pushing model weights

--- pumba_testing.py
+++ pumba_testing.py
@@ -5,7 +5,7 @@ import tensorflow.keras.backend as K
 import numpy as np
 from dipy.io.image import load_nifti, save_nifti
 from dipy.align.reslice import reslice
-from dipy.nn.utils import pad_crop
+from dipy.nn.utils import pad_crop, inv_pad_crop, get_bounds
 from scipy.ndimage import affine_transform, label
 
 from skimage.morphology import binary_dilation
@@ -39,18 +39,14 @@ def dice_coefficient(y_true, y_pred):
 def dice_loss(y_true, y_pred):
     return 1 - dice_coefficient(y_true, y_pred)
 
-
 def transform_img(
     image,
     affine,
     *,
-    voxsize=(1, 1, 1),
     target_voxsize=None,
+    final_size=None,
+    order=3,
     considered_points="corners",
-    init_shape=None,
-    ratio=None,
-    set_size=None,
-    need_isotropic=False,
 ):
     """
     Function to transform images for Deep Learning models
@@ -61,140 +57,92 @@ def transform_img(
         Image to transform.
     affine : np.ndarray
         Affine matrix provided by the image file.
-    voxsize : tuple (3,)
-        Voxel size provided by the image file.
-    target_voxsize : tuple (3,)
+    target_voxsize : tuple (3,), optional
         The voxel size we want to start from.
-        Ignored if need_isotropic is True.
-    considered_points : str
+        If none is provided, we calculate the closest isotropic voxel size.
+    final_size : tuple (3,), optional
+        The final size of the image array.
+    order : int, optional
+        The order of the spline interpolation.
+        The order has to be in the range 0-5.
+        If transforming an int image, order 0 is recommended.
+    considered_points : str, optional
         Considered points when calculating the transformed shape.
         \"corners\" will consider only corners of the image shape.
         \"all\" will consider all voxels. Might be needed when shearing is applied.
-    init_shape : tuple (3,)
-        What we want the initial shape to be before last resizing step.
-        Ignored if need_isotropic is True.
-    ratio : float
-        The ratio of change in the last resizing step.
-        Ignored if need_isotropic is True.
-    set_size : tuple (3,)
-        The final size of the image array.
-    need_isotropic : bool
-        Whether the output needs to be isotropic in the end.
-
-
 
     Returns
     -------
     new_image : np.ndarray
         Transformed image to be used in the Deep Learning model.
-    params : tuple
+    params : dict
         Parameters that are used when recovering the original image space.
     """
     ori_shape = image.shape
-    if need_isotropic:
-        target_voxsize = tuple(np.max(voxsize) * np.ones(3))
-    if target_voxsize is not None and np.any(target_voxsize != np.ones(3)):
-        image2, affine2 = reslice(image, affine, voxsize, target_voxsize)
+
+    R = affine[:3, :3]
+    voxsize = np.sqrt(np.sum(R * R, axis=0))
+    if target_voxsize is None:
+        target_voxsize = np.array([np.min(voxsize)] * 3)
     else:
-        image2 = image.copy()
-        affine2 = affine.copy()
+        target_voxsize = np.array(target_voxsize)
 
-    shape = image2.shape
-
-    if considered_points == "corners":
-        corners = np.array(
-            [
-                [0, 0, 0, 1],
-                [shape[0] - 1, 0, 0, 1],
-                [0, shape[1] - 1, 0, 1],
-                [0, 0, shape[2] - 1, 1],
-                [shape[0] - 1, shape[1] - 1, shape[2] - 1, 1],
-                [shape[0] - 1, 0, shape[2] - 1, 1],
-                [0, shape[1] - 1, shape[2] - 1, 1],
-                [shape[0] - 1, shape[1] - 1, 0, 1],
-            ],
-            dtype=np.float64,
+    if not np.allclose(voxsize, target_voxsize):
+        image, resliced_affine = reslice(
+            image, affine, tuple(voxsize), tuple(target_voxsize)
         )
-    elif considered_points == "all":
-        temp1 = np.arange(shape[0])
-        temp2 = np.arange(shape[1])
-        temp3 = np.arange(shape[2])
-        grid1, grid2, grid3 = np.meshgrid(temp1, temp2, temp3)
-        corners = np.vstack([grid1.ravel(), grid2.ravel(), grid3.ravel()]).T
-        corners = np.hstack([corners, np.full((corners.shape[0], 1), 1)])
-        corners = corners.astype(np.float64)
     else:
-        ValueError('considered points should be "corners" or "all"')
+        resliced_affine = None
 
-    transformed_corners = (affine2 @ corners.T).T
-    min_bounds = transformed_corners.min(axis=0)[:3]
-    max_bounds = transformed_corners.max(axis=0)[:3]
+    init_shape = image.shape
 
-    # Calculate the required offset to ensure
-    # all necessary coordinates are positive
+    min_bounds, max_bounds = get_bounds(
+        init_shape, affine, considered_points=considered_points
+    )
+
     offset = np.ceil(-min_bounds)
     new_shape = (np.ceil(max_bounds) + offset).astype(int)
     offset_array = np.array(
         [[1, 0, 0, offset[0]], [0, 1, 0, offset[1]], [0, 0, 1, offset[2]], [0, 0, 0, 1]]
     )
 
-    new_affine = affine2.copy()
+    new_affine = affine.copy()
     new_affine = np.matmul(offset_array, new_affine)
 
     inv_affine = np.linalg.inv(new_affine)
+
     new_image = np.zeros(tuple(new_shape))
     affine_transform(
-        image2, inv_affine, output_shape=tuple(new_shape), output=new_image
+        image,
+        inv_affine,
+        output_shape=tuple(new_shape),
+        output=new_image,
+        order=order,
     )
-
-    mid_image = new_image.copy()
 
     crop_vs = None
     pad_vs = None
-    if not need_isotropic:
-        if init_shape:
-            new_image, pad_vs, crop_vs = pad_crop(new_image, init_shape)
+    if final_size is None:
+        final_size = new_image.shape
+    final_size = np.array(final_size)
+    new_image, pad_vs, crop_vs = pad_crop(new_image, final_size)
 
-        if (ratio is not None and ratio != 1) and set_size is None:
-            new_image, _ = reslice(
-                new_image, np.eye(4), (1, 1, 1), (ratio, ratio, ratio)
-            )
-        elif set_size:
-            new_image, _ = reslice(
-                new_image,
-                np.eye(4),
-                (1, 1, 1),
-                (
-                    new_image.shape[0] / set_size[0],
-                    new_image.shape[1] / set_size[1],
-                    new_image.shape[2] / set_size[2],
-                ),
-            )
-
-    else:
-        ratio = np.max(np.array(mid_image.shape) / np.array(set_size))
-        new_size = np.ceil(np.array(set_size) * ratio)
-        new_image, pad_vs, crop_vs = pad_crop(mid_image, tuple(new_size))
-        new_image, _ = reslice(new_image, np.eye(4), (1, 1, 1), (ratio, ratio, ratio))
-
-    params = (
-        inv_affine,
-        image2.shape,
-        offset_array,
-        crop_vs,
-        pad_vs,
-        ratio,
-        voxsize,
-        target_voxsize,
-        need_isotropic,
-        set_size,
-        ori_shape,
-    )
+    params = {
+        "inv_affine": inv_affine,
+        "offset": offset_array,
+        "crop_value": crop_vs,
+        "pad_value": pad_vs,
+        "voxsize": voxsize,
+        "target_voxsize": target_voxsize,
+        "ori_shape": ori_shape,
+        "init_shape": init_shape,
+        "affine": resliced_affine,
+    }
 
     return new_image, params
 
 
-def recover_img(image, params):
+def recover_img(image, params, *, order=3):
     """
     Function to recover image from transform_img
 
@@ -205,6 +153,10 @@ def recover_img(image, params):
     params : tuple
         Parameters for recover_img function.
         Returned from transform_img.
+    order : int, optional
+        The order of the spline interpolation.
+        The order has to be in the range 0-5.
+        If recovering an int image, order 0 is recommended.
 
     Returns
     -------
@@ -214,63 +166,46 @@ def recover_img(image, params):
         Recovered affine.
         This should be same as the original affine.
     """
-    (
-        inv_affine,
-        mid_shape,
-        offset_array,
-        crop_vs,
-        pad_vs,
-        ratio,
-        voxsize,
-        target_voxsize,
-        need_isotropic,
-        set_size,
-        ori_shape,
-    ) = params
-    new_affine = np.linalg.inv(inv_affine)
-    if need_isotropic:
-        new_image, _ = reslice(image, np.eye(4), (ratio, ratio, ratio), (1, 1, 1))
-    else:
-        if (ratio is not None and ratio != 1) and set_size is None:
-            new_image, _ = reslice(image, np.eye(4), (ratio, ratio, ratio), (1, 1, 1))
-        elif set_size:
-            new_image, _ = reslice(
-                image,
-                np.eye(4),
-                (
-                    image.shape[0] / set_size[0],
-                    image.shape[1] / set_size[1],
-                    image.shape[2] / set_size[2],
-                ),
-                (1, 1, 1),
-            )
-        else:
-            new_image = image
+    expected_keys = {
+        "inv_affine",
+        "crop_value",
+        "pad_value",
+        "voxsize",
+        "target_voxsize",
+        "ori_shape",
+        "init_shape",
+        "affine",
+    }
+    missing = expected_keys - set(params.keys())
+    if missing:
+        raise ValueError(f"params is missing keys: {missing}")
+
+    inv_affine = params["inv_affine"]
+    crop_vs = params["crop_value"]
+    pad_vs = params["pad_value"]
+    voxsize = params["voxsize"]
+    target_voxsize = params["target_voxsize"]
+    ori_shape = params["ori_shape"]
+    init_shape = params["init_shape"]
+    affine = params["affine"]
 
     if crop_vs is not None and pad_vs is not None:
-        crop_vs = crop_vs.astype(int)
-        pad_vs = pad_vs.astype(int)
-        new_image = np.pad(
+        new_image = inv_pad_crop(image, crop_vs, pad_vs)
+
+    new_affine = np.linalg.inv(inv_affine)
+    new_image = affine_transform(
+        new_image, new_affine, output_shape=init_shape, order=order
+    )
+
+    if not np.allclose(voxsize, target_voxsize):
+        new_image, affine = reslice(
             new_image,
-            (
-                (crop_vs[0, 0], crop_vs[0, 1]),
-                (crop_vs[1, 0], crop_vs[1, 1]),
-                (crop_vs[2, 0], crop_vs[2, 1]),
-            ),
+            affine,
+            tuple(target_voxsize),
+            tuple(voxsize),
+            new_shape=ori_shape,
+            order=order,
         )
-        new_image = new_image[
-            pad_vs[0, 0] : new_image.shape[0] - pad_vs[0, 1],
-            pad_vs[1, 0] : new_image.shape[1] - pad_vs[1, 1],
-            pad_vs[2, 0] : new_image.shape[2] - pad_vs[2, 1],
-        ]
-
-    new_image = affine_transform(new_image, new_affine, output_shape=mid_shape)
-    affine = np.matmul(np.linalg.inv(offset_array), new_affine)
-
-    if target_voxsize is not None and np.any(target_voxsize != np.ones(3)):
-        new_image, affine = reslice(new_image, affine, target_voxsize, voxsize)
-        if new_image.shape != ori_shape:
-            new_image = pad_crop(new_image, ori_shape)
 
     return new_image, affine
 
@@ -280,14 +215,15 @@ if __name__ == "__main__":
 
     file_name = ''
     output_name = ''
-    image, affine, voxsize = load_nifti(file_name, return_voxsize=True)
+    image, affine = load_nifti(file_name, return_voxsize=True)
     image = np.interp(image, (np.percentile(image, 1), np.percentile(image, 99)), (0, 1))
-    image, params = transform_img(image, affine, (128, 128, 128))
+    image, params = transform_img(image, affine, (2, 2, 2), (128, 128, 128))
     shape = image.shape
 
     pred = np.squeeze(model(np.reshape(image, (1, 128, 128, 128, 1))))
 
     pred = post_process(pred)
 
-    pred = recover_img(pred, params)
-    save_nifti(output_name, np.round(pred), affine)
+    pred, _ = recover_img(pred, params)
+
+    save_nifti(output_name, np.round(pred).astype(np.int32), affine)
